f5065256cf74b113146580ea9ccceae6b3fd6c5d
atomassoni <annetomassoni@gmail.com>
2016-06-23 10:32:35 -0500

    dependency update, added models and routes

---
+++ server/models/job.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var EmailSchema = require('./email').schema;
+
+// Mongoose Schema
+var JobSchema = new Schema({
+    name: {type: String, required: true, index: {unique: true}},
+    email: [EmailSchema],
+    phoneNumber: {type: String, required: true},
+    squareFeet: {type: String},
+    dueDate: {type: Date},
+    timeFrame: {type: String},
+    afterDark: {type: Number},
+    preferredDate: {type: Date},
+    notes: {type: String},
+    entryMethod: {type: String}
+
+});
+
+
+module.exports = Job;
