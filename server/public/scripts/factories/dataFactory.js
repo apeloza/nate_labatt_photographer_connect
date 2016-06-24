636084585942e2e15ed13ea2df3f35be838bcaa6
apeloza <apeloza@alumni.uwo.ca>
2016-06-24 11:11:25 -0500

    MyJobs functionality added

--- server/public/scripts/factories/dataFactory.js
+++ server/public/scripts/factories/dataFactory.js
@@ -2,12 +2,30 @@ app.factory('DataFactory', ['$http', function($http) {
     console.log('Data Factory running.');
 
     var user;
+    var jobs;
+    var userJobs = [];
     //PRIVATE
     function authenticate() {
         var promise = $http.get('/user').then(function(response) {
             user = response.data;
         });
         return promise;
+    }
+
+    function getAllJobs() {
+        var promise = $http.get('/jobs/alljobs').then(function(response) {
+            jobs = response.data;
+        });
+        return promise;
+    }
+
+    function findUserJobs() {
+        for (var i = 0; i < jobs.length; i++) {
+            if (jobs[i].jobAcceptedBy == user.username) {
+                userJobs.push(jobs[i]);
+            }
+        }
+        return userJobs;
     }
 
     function storeUsername() {
@@ -21,7 +39,9 @@ app.factory('DataFactory', ['$http', function($http) {
     var publicFunctions = {
         authenticate: authenticate,
         storeUsername: storeUsername,
-        storeUserLevel: storeUserLevel
+        storeUserLevel: storeUserLevel,
+        getAllJobs: getAllJobs,
+        findUserJobs: findUserJobs
     };
     return publicFunctions;
 }]);
