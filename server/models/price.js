8d48be50d8f535254e319eb8b3c2ef8463795641
apeloza <apeloza@alumni.uwo.ca>
2016-06-30 11:21:33 -0500

    Fix for pricing model

--- server/models/price.js
+++ server/models/price.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PriceSchema = new Schema({
-  name: {type: String, required: true},
-  value: {type: String, required: true},
-  group: {type: String}
+  sqft: {type: Array, required: true},
+  afterDark: {type: Array, required: true},
+  addons: {type: Array}
 });
 
 module.exports = mongoose.model('Price', PriceSchema);
