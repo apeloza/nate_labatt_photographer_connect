7e837232cc44b028d4af73a7ecc047ad31a0c1ba
atomassoni <annetomassoni@gmail.com>
2016-07-05 13:15:40 -0500

    email

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -17,7 +17,7 @@ router.post('/', function(req, res) {
     var subject = req.body.subject;
     var jobID = req.body.jobID;
     var message = 'Hello,\n' +
-'You’re receiving this message from a service called Pixel Houz! We help property owners connect with real estate photographers to set a time for a photo session. You indicated to your realtor that your preferred date and time are <strong>$preferred date$ in the $preferred time$ </strong>. \n\n' +
+'You’re receiving this message from a service called Pixel Houz! We help property owners connect with real estate photographers to set a time for a photo session. You indicated to your realtor that your preferred date and time are $preferred date$ in the $preferred time$ . \n\n' +
 
 'Reply to this email to communicate directly with the photographer to find a time that works for both of you. \n\n' +
 
@@ -64,9 +64,9 @@ router.get('/messages', function(req, res) {
 
             console.log(error);
             console.log(body);
-
+            if (body) {
             res.send(body);
-
+        }
         });
 
 });
@@ -91,6 +91,7 @@ router.post('/messages/received', function(req, res) {
     console.log(req.body);
 
 if (body) {
+    console.log(body);
     res.sendStatus(200);
 } else {
     res.sendStatus(406);
