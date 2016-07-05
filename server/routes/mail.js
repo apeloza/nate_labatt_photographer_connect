c073a82d9c6d354b5d4db15b798f9455580d45ea
atomassoni <annetomassoni@gmail.com>
2016-07-04 20:44:45 -0500

    mailgun variables

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -29,7 +29,7 @@ router.post('/', function(req, res) {
             jobID: jobID
         };
             //attachment: filepath
-    };
+    
 
     //   var transporter = nodemailer.createTransport({
     //       service: 'Mailgun',
