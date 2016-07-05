4f89e74cb442a5e85d624334c48659c953011d10
apeloza <apeloza@alumni.uwo.ca>
2016-07-05 13:26:29 -0500

    Now checks job status before accepting job

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 var Job = require('../models/job');
 var path = require('path');
 
+//Gets a list of all jobs.
 router.get('/', function(req, res) {
     if (req.isAuthenticated()) {
         Job.find({}, function(err, jobs) {
@@ -32,31 +33,37 @@ router.post('/', function(req, res, next) {
         res.send(false);
     }
 });
+
+//Updates a job to accepted status. Checks to see if the job is taken before changes are made.
 router.put('/:id', function(req, res) {
 
-  console.log(req.body);
-
-
-  if(req.isAuthenticated()){
     console.log(req.body);
 
-    Job.findOne({
-        _id: req.params.id
-    }, function(err, job) {
-        job.jobStatus = "accepted";
-        job.jobAcceptedBy = req.body.username;
-        job.save(function(err) {
 
-          if(err){
-            console.log(err);
-          }
-          res.send(job);
+    if (req.isAuthenticated()) {
+        console.log(req.body);
 
+        Job.findOne({
+            _id: req.params.id
+        }, function(err, job) {
+            if (job.jobStatus == "open") {
+                job.jobStatus = "accepted";
+                job.jobAcceptedBy = req.body.username;
+                job.save(function(err) {
+
+                    if (err) {
+                        console.log(err);
+                    }
+                    res.send(job);
+
+                });
+            } else {
+                res.sendStatus(500);
+            }
         });
-    });
-  } else {
-    res.send(false);
-  }
+    } else {
+        res.send(false);
+    }
 });
 router.put('/finish/:id', function(req, res) {
     if (req.isAuthenticated()) {
@@ -77,55 +84,55 @@ router.put('/finish/:id', function(req, res) {
     }
 });
 router.put('/reopen/:id', function(req, res) {
-  var id = req.params.id;
-  if(req.isAuthenticated()){
-    console.log(req.body);
+    var id = req.params.id;
+    if (req.isAuthenticated()) {
+        console.log(req.body);
 
-    Job.findOne({
-        _id: id
-    }, function(err, job) {
-        job.jobStatus = "open";
-        job.jobAcceptedBy = '';
-        job.save(function(err) {
+        Job.findOne({
+            _id: id
+        }, function(err, job) {
+            job.jobStatus = "open";
+            job.jobAcceptedBy = '';
+            job.save(function(err) {
 
-          if(err){
-            console.log(err);
-          }
-            res.sendStatus(204);
+                if (err) {
+                    console.log(err);
+                }
+                res.sendStatus(204);
 
+            });
         });
-    });
-  } else {
-    res.send(false);
-  }
+    } else {
+        res.send(false);
+    }
 });
 
 router.delete('/:id', function(req, res) {
-  if(req.isAuthenticated()){
-    Job.findByIdAndRemove(req.params.id, function(err) {
-        if (err) {
-            res.sendStatus(500);
-            return;
-        }
-        res.sendStatus(204);
-    });
-  } else {
-    res.send(false);
-  }
+    if (req.isAuthenticated()) {
+        Job.findByIdAndRemove(req.params.id, function(err) {
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
+            res.sendStatus(204);
+        });
+    } else {
+        res.send(false);
+    }
 });
 router.get('/alljobs', function(req, res) {
-  if(req.isAuthenticated()){
-    Job.find({}, function(err, data) {
-        if (err) {
-            res.sendStatus(500);
-            return;
-        }
+    if (req.isAuthenticated()) {
+        Job.find({}, function(err, data) {
+            if (err) {
+                res.sendStatus(500);
+                return;
+            }
 
-        res.send(data);
-    });
-  } else {
-    res.send(false);
-  }
+            res.send(data);
+        });
+    } else {
+        res.send(false);
+    }
 });
 
 module.exports = router;
